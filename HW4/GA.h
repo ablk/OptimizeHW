d5023f84a92fa33fd5a4208f507b098f0d807467
ablk <xu6u3vup@gmail.com>
2019-12-20 05:39:20 +0800

    HW4

---
+++ HW4/GA.h
@@ -0,0 +1,196 @@
+#include <iostream>
+#include <cmath>
+#include <Eigen/Core>
+#include <random>
+class GA{
+    public:
+        Eigen::MatrixXd chromo;
+        Eigen::VectorXd minx;
+        Eigen::VectorXd maxx;
+        int dim;
+        int chromonum;
+        double crossoverrate;
+        double alpha;//0~1 : higher -> offspring vary more
+        double mutationrate;
+        double mp;//about 20~100 : lower -> expand mutation range
+        
+        bool usedecoder;
+        Eigen::VectorXd (*decode)(const Eigen::VectorXd &x);
+        
+        double (*f)(const Eigen::VectorXd &x);
+        std::default_random_engine re;
+        
+        GA(double (*F)(const Eigen::VectorXd &x),int Dim,int ChromoNum,
+            const Eigen::VectorXd MinX,const Eigen::VectorXd MaxX,
+            double CrossOverRate,double CrossOverAlpha,
+            double MutationRate,double MutationParam);
+        void Init();
+        void Solve(int Iteration,bool verbose);
+        void Mutation();
+        void Crossover();
+        void Selection();
+        void ShowResult();
+        void SetDecoder(Eigen::VectorXd (*Decode)(const Eigen::VectorXd &x));
+};
+
+
+GA::GA(double (*F)(const Eigen::VectorXd &x),int Dim,int ChromoNum,
+            const Eigen::VectorXd MinX,const Eigen::VectorXd MaxX,
+            double CrossOverRate,double CrossOverAlpha,
+            double MutationRate,double MutationParam){
+    dim=Dim;
+    chromonum=ChromoNum;
+    crossoverrate=CrossOverRate;
+    mutationrate=MutationRate;
+    minx=MinX;
+    maxx=MaxX;
+    f=F;
+    alpha=CrossOverAlpha;
+    mp=MutationParam;
+    Init();
+    usedecoder=false;
+}
+
+void GA::SetDecoder(Eigen::VectorXd (*Decode)(const Eigen::VectorXd &x)){
+    decode=Decode;
+    usedecoder=true;
+}
+
+void GA::Init(){
+    chromo=Eigen::MatrixXd(dim,chromonum);
+    // to obtain one chromo: chromo.col(j)
+    for(int i=0;i<dim;i++){
+        std::uniform_real_distribution<double> rd(minx(i),maxx(i));
+        for(int j=0;j<chromonum;j++){
+            chromo(i,j) = rd(re);
+        }
+    }
+    
+}
+void GA::Solve(int Iteration,bool verbose){
+    for(int i=0;i<Iteration;i++){
+        Selection();
+
+        Crossover();
+
+        Mutation();
+        
+        if(i%10==0 && verbose){
+            std::cout<<"iteration="<<i<<std::endl;
+            ShowResult();
+        }
+
+    }
+}
+
+void GA::Selection(){
+    std::uniform_int_distribution<> indexgen(0, chromonum-1);
+    for(int i=0;i<chromonum;i++){
+        int idx=indexgen(re);
+        //minimize
+        double vidx,vi;
+        if(usedecoder){
+            vidx=(*f)((*decode)(chromo.col(idx)));
+            vi=(*f)((*decode)(chromo.col(i)));
+        }
+        else{
+            vidx=(*f)(chromo.col(idx));
+            vi=(*f)(chromo.col(i));
+        }
+        if(vidx<vi){
+            chromo.col(i)=chromo.col(idx);
+        }
+    }
+}
+
+void GA::Crossover(){
+    //Blend crossover alpha 
+    std::uniform_int_distribution<> indexgen(0, chromonum-1);
+    std::uniform_real_distribution<double> rd(0,1);
+    for(int i=0;i<chromonum;i++){
+        if(rd(re)>crossoverrate)continue;
+        int idx1,idx2;
+        idx1=indexgen(re);
+        idx2=indexgen(re);
+        
+        Eigen::VectorXd x1=chromo.col(idx1);
+        Eigen::VectorXd x2=chromo.col(idx2);
+        Eigen::VectorXd d(dim);
+        Eigen::VectorXd min12(dim); //min max of x1 x2
+        Eigen::VectorXd max12(dim);
+        
+        for(int j=0;j<dim;j++){
+            if(x1(j)<x2(j)){
+                min12(j)=x1(j);
+                max12(j)=x2(j);
+            }
+            else{
+                min12(j)=x2(j);
+                max12(j)=x1(j);
+            }
+        }
+        d=max12-min12;
+        min12=min12-alpha*d;
+        max12=max12+alpha*d;
+        
+        for(int j=0;j<dim;j++){
+            std::uniform_real_distribution<double> rdof(min12(j),max12(j));
+            chromo(j,idx1)=rdof(re);
+            if(chromo(j,idx1)<minx(j)){
+                chromo(j,idx1)=minx(j);
+            }
+            else if(chromo(j,idx1)>maxx(j)){
+                chromo(j,idx1)=maxx(j);
+            }
+            chromo(j,idx2)=rdof(re);
+            if(chromo(j,idx2)<minx(j)){
+                chromo(j,idx2)=minx(j);
+            }
+            else if(chromo(j,idx2)>maxx(j)){
+                chromo(j,idx2)=maxx(j);
+            }
+        }
+    }
+    
+}
+void GA::Mutation(){
+    //Polynomial Mutation
+    std::uniform_int_distribution<> indexgen(0, chromonum-1);
+    std::uniform_real_distribution<double> rd(0,1);
+    for(int i=0;i<chromonum;i++){
+        if(rd(re)>mutationrate)continue;
+        int idx=indexgen(re);
+        for(int j=0;j<dim;j++){
+            double u=rd(re);
+            if(u>0.5){//upper
+                double deltaU=1-pow(2.0*(1.0-u),1.0/(1.0+mp));
+                chromo(j,idx)=chromo(j,idx)+deltaU*(maxx(j)-chromo(j,idx));
+            }
+            else{//lower
+                double deltaL=pow(2*u,1.0/(1.0+mp))-1;
+                chromo(j,idx)=chromo(j,idx)+deltaL*(chromo(j,idx)-minx(j));
+            }
+        }
+    }
+}
+
+void GA::ShowResult(){
+    double m=usedecoder?(*f)((*decode)(chromo.col(0))) : (*f)(chromo.col(0));
+    int idx=0;
+    for(int i=1;i<chromonum;i++){
+        double v=usedecoder?(*f)((*decode)(chromo.col(i))) : (*f)(chromo.col(i));
+
+        if(v<m){
+            m=v;
+            idx=i;
+        }
+    }
+    std::cout<<"min value:"<<m<<std::endl;
+    if(usedecoder){
+        std::cout<<"\tx:"<<(*decode)(chromo.col(idx)).transpose()<<std::endl;
+    }
+    else{
+        std::cout<<"\tx:"<<chromo.col(idx).transpose()<<std::endl;
+    }
+    std::cout<<"------------------------------------------"<<std::endl;
+}
---
+++ HW4/HW4.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <cmath>
+#include <Eigen/Core>
+#include <Eigen/Sparse>
+#include <fstream>
+#include <random>
+#include <ctime>
+//build:
+//under HW4 folder
+//g++ -I../Eigen HW4.cpp -o HW4
+
+//execute:
+//HW4 problem_number
+//for example, to solve Problem_1_1: HW4 1
+#include "GA.h"
+
+double Problem_1_1(const Eigen::VectorXd &x){
+    return x(0)*sin(x(0))+x(1)*sin(5*x(1));
+}
+
+double Problem_1_2_a(const Eigen::VectorXd &x){    
+    return -exp(0.2*sqrt(pow(x(0)-1,2)+pow(x(1)-1,2))+cos(2*x(0))+sin(2*x(0)));
+}
+
+double Problem_1_2_b(const Eigen::VectorXd &x){
+    double dotx=x.dot(x);
+    return 0.5+(sin(dotx)-0.5)/(1+0.1*dotx);
+}
+
+Eigen::VectorXd Decode_1_2_b(const Eigen::VectorXd &x){
+    Eigen::VectorXd r(x.size());
+    for(int i=0;i<x.size();i++){
+        r(i)=tan(x(i));
+    }
+    return r;
+}
+int main(int argc, char * argv[])
+{
+    int p=3;
+    if(argc>=2){
+        p=atoi(argv[1]);
+    }
+    if(p==1){
+        std::cout<<"============="<<std::endl;
+        std::cout<<"=Problem_1_1="<<std::endl;
+        std::cout<<"============="<<std::endl;
+        int Dim=2;
+        int ChromoNum=100;
+        Eigen::VectorXd MinX(Dim),MaxX(Dim);
+        MinX<<0,4;
+        MaxX<<10,6;
+        
+        double CrossOverRate=0.5;
+        double CrossOverAlpha=0.386;
+        double MutationRate=0.2;
+        double MutationParam=20;
+        
+        
+        GA ga(&Problem_1_1, Dim, ChromoNum,
+                MinX,MaxX,
+                CrossOverRate, CrossOverAlpha,
+                MutationRate, MutationParam);
+        ga.Solve(100,true);
+        ga.ShowResult();
+    }
+    else if(p==2){
+        std::cout<<"==============="<<std::endl;
+        std::cout<<"=Problem_1_2_a="<<std::endl;
+        std::cout<<"==============="<<std::endl;
+        int Dim=2;
+        int ChromoNum=100;
+        Eigen::VectorXd MinX(Dim),MaxX(Dim);
+        MinX<<-5,-5;
+        MaxX<<5,5;
+        
+        double CrossOverRate=0.5;
+        double CrossOverAlpha=0.386;
+        double MutationRate=0.2;
+        double MutationParam=20;
+        
+        
+        GA ga(&Problem_1_2_a, Dim, ChromoNum,
+                MinX,MaxX,
+                CrossOverRate, CrossOverAlpha,
+                MutationRate, MutationParam);
+        ga.Solve(100,true);
+        ga.ShowResult();
+    }
+    else{
+        std::cout<<"==============="<<std::endl;
+        std::cout<<"=Problem_1_2_b="<<std::endl;
+        std::cout<<"==============="<<std::endl;
+        int Dim=2;
+        int ChromoNum=100;
+        Eigen::VectorXd MinX(Dim),MaxX(Dim);
+        MinX<<-M_PI/2,-M_PI/2;
+        MaxX<<M_PI/2,M_PI/2;
+        
+        double CrossOverRate=0.5;
+        double CrossOverAlpha=0.386;
+        double MutationRate=0.2;
+        double MutationParam=20;
+        
+        
+        GA ga(&Problem_1_2_b, Dim, ChromoNum,
+                MinX,MaxX,
+                CrossOverRate, CrossOverAlpha,
+                MutationRate, MutationParam);
+        ga.SetDecoder(Decode_1_2_b);
+        ga.Solve(100,true);
+        ga.ShowResult();
+    }
+}
